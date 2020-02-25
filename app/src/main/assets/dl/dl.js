73d2d6a437b0db8c699536142beb95fe08f1fe8b
HAMANO Tsukasa <hamano@osstech.co.jp>
2020-02-25 20:36:59 +0900

    add dlstring.js

--- app/src/main/assets/dl/dl.js
+++ app/src/main/assets/dl/dl.js
@@ -29,25 +29,6 @@ function full2half(str) {
     });
 }
 
-function dlstr2html(str) {
-    if (typeof str == "string") {
-        return str;
-    }
-    var html = "";
-    const style = 'height: 1em; position: relative; top: 0.14em;';
-    for (c of str) {
-        if (c['type'] == 'text/plain') {
-            html += htmlEscape(c['value']);
-        } else if (c['type'] == 'image/png') {
-            html += '<img style="' + style + '" ' +
-                'src="data:image/png;base64,' + c['value'] +  '" />';
-        } else {
-            // missing
-        }
-    }
-    return html;
-}
-
 function render(json) {
     data = JSON.parse(json);
     if ('dl-name' in data) {
---
+++ app/src/main/assets/dl/dlstring.js
@@ -0,0 +1,19 @@
+function dlstr2html(str) {
+    if (typeof str == "string") {
+        return str;
+    }
+    var html = "";
+    const style = 'height: 1em; position: relative; top: 0.14em;';
+    for (c of str) {
+        if (c['type'] == 'text/plain') {
+            html += htmlEscape(c['value']);
+        } else if (c['type'] == 'image/png') {
+            html += '<img style="' + style + '" ' +
+                'src="data:image/png;base64,' + c['value'] +  '" />';
+        } else {
+            // missing
+        }
+    }
+    return html;
+}
+
